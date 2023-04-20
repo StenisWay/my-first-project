e0bbfeb5c8285dbb31c37a026cdafc372534121b
Brian <qazse753753@gmail.com>
2023-04-20 11:45:23 +0800

    Project init 喵喵~~~~~ 嗷嗷~~~~~ 汪汪汪~~~~~ 隆隆隆~~~

---
+++ src/main/java/test/TestServlet.java
@@ -0,0 +1,20 @@
+package test;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/TestServlet")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setCharacterEncoding("UTF-8");
+		response.getWriter().append("就只是一個測試而已");
+	}
+
+}
